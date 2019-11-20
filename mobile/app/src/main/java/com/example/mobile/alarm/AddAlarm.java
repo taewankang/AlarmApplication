34956b149c5bb587fdf2928a97650e9320c42616
taewankang <king6096211@naver.com>
2019-11-20 20:45:22 +0900

    2019.11.20

--- mobile/app/src/main/java/com/example/mobile/alarm/AddAlarm.java
+++ mobile/app/src/main/java/com/example/mobile/alarm/AddAlarm.java
@@ -1,26 +1,100 @@
 package com.example.mobile.alarm;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.mobile.R;
 
 public class AddAlarm extends AppCompatActivity {
-
+    ImageView monday_imageView, tuesday_imageView, wednesday_imageView, thursday_imageView, friday_imageView,
+    saturday_imageView, sunday_imageView;
+    boolean[] day_arr = new boolean[7];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
+        monday_imageView = (ImageView)findViewById(R.id.monday_imageView);
+        tuesday_imageView = (ImageView)findViewById(R.id.tuesday_imageView);
+        wednesday_imageView = (ImageView)findViewById(R.id.wednesday_imageView);
+        thursday_imageView = (ImageView)findViewById(R.id.thursday_imageView);
+        friday_imageView = (ImageView)findViewById(R.id.friday_imageView);
+        saturday_imageView = (ImageView)findViewById(R.id.saturday_imageView);
+        sunday_imageView = (ImageView)findViewById(R.id.sunday_imageView);
+        for(int i=0; i<7; i++)
+            day_arr[i] = false;
     }
 
-    public void cancel(){
+    public void click_sunday(View view) {
+        if(day_arr[0] == false)
+            sunday_imageView.setVisibility(View.VISIBLE);
+        else
+            sunday_imageView.setVisibility(View.INVISIBLE);
+        day_arr[0] = !day_arr[0];
+    }
+
+    public void click_monday(View view) {
+        if (day_arr[1] == false)
+            monday_imageView.setVisibility(View.VISIBLE);
+        else
+            monday_imageView.setVisibility(View.INVISIBLE);
+        day_arr[1] = !day_arr[1];
+    }
+
+    public void click_saturday(View view) {
+        if(day_arr[6] == false)
+            saturday_imageView.setVisibility(View.VISIBLE);
+        else
+            saturday_imageView.setVisibility(View.INVISIBLE);
+        day_arr[6] = !day_arr[6];
+    }
+
+    public void click_friday(View view) {
+        if(day_arr[5] == false)
+            friday_imageView.setVisibility(View.VISIBLE);
+        else
+            friday_imageView.setVisibility(View.INVISIBLE);
+        day_arr[5] = !day_arr[5];
+    }
+
+    public void click_thursday(View view) {
+        if(day_arr[4] == false)
+            thursday_imageView.setVisibility(View.VISIBLE);
+        else
+            thursday_imageView.setVisibility(View.INVISIBLE);
+        day_arr[4] = !day_arr[4];
+    }
+
+    public void click_wednesday(View view) {
+        if(day_arr[3] == false)
+            wednesday_imageView.setVisibility(View.VISIBLE);
+        else
+            wednesday_imageView.setVisibility(View.INVISIBLE);
+        day_arr[3] = !day_arr[3];
+    }
+
+    public void click_tuesday(View view) {
+        if(day_arr[2] == false)
+            tuesday_imageView.setVisibility(View.VISIBLE);
+        else
+            tuesday_imageView.setVisibility(View.INVISIBLE);
+        day_arr[2] = !day_arr[2];
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+
+    public void save(View view) {
 
     }
 
-    public void save(){
-
+    public void Dialog_calendar(View view) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getApplicationContext());
     }
-
 }
--- mobile/app/src/main/java/com/example/mobile/alarm/Alarm.java
+++ mobile/app/src/main/java/com/example/mobile/alarm/Alarm.java
@@ -6,23 +6,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
-
 import com.example.mobile.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
 public class Alarm extends Fragment {
-    Button add, more;
+    Button add, more, alarm_add_button;
+    TextView alarm_textView;
+    LinearLayout linearLayout;
+    List<String> list = new ArrayList<>();
     public View onCreateView(LayoutInflater inflate, ViewGroup container, Bundle savedinstanceState){
         View view = inflate.inflate(R.layout.alarm, container, false);
         add = (Button)view.findViewById(R.id.add);
         more = (Button)view.findViewById(R.id.more);
-
+        alarm_textView = (TextView)view.findViewById(R.id.alarm_textView);
+        linearLayout = (LinearLayout)view.findViewById(R.id.linearLayout);
+        alarm_add_button = (Button)view.findViewById(R.id.alarm_add_button);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getContext(), AddAlarm.class);
-                startActivityForResult(intent, 0);
+                addAlarm();
             }
         });
 
@@ -34,12 +45,30 @@ public class Alarm extends Fragment {
                 alertDialog.setMessage("추가");
             }
         });
+
+        alarm_add_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                addAlarm();
+            }
+        });
         return view;
     }
 
     @Override
     public void onStart(){
         super.onStart();
+        if(list.size() == 0){
+            alarm_textView.setVisibility(GONE);
+            add.setVisibility(GONE);
+        }else{
+            add.setVisibility(VISIBLE);
+            linearLayout.setVisibility(GONE);
+        }
+    }
 
+    public void addAlarm(){
+        Intent intent = new Intent(getContext(), AddAlarm.class);
+        startActivityForResult(intent, 0);
     }
 }
